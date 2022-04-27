31209ffc4eff4c19cd63d934a385d498a2103346
Jina Kim <kjn0265@gmail.com>
2022-04-27 19:41:01 +0900

    simple peer 추가

--- src/pages/Gameroom.js
+++ src/pages/Gameroom.js
@@ -1,60 +1,75 @@
-import styled from "styled-components"
-import Grid from "../element/Grid";
-import Button from "../element/Button";
-import Chatdiv from'../component/Chatdiv';
+import styled from 'styled-components'
+import Grid from '../element/Grid'
+import Button from '../element/Button'
+import Chatdiv from '../component/Chatdiv'
+import VideoChat from '../component/VideoChat'
 
-import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
-function GameRoom(props){
-    const socket = useSelector(state => state.post.data);
-    const currentId = localStorage.getItem('userId')
+function GameRoom(props) {
+  const socket = useSelector((state) => state.post.data)
+  const currentId = localStorage.getItem('userId')
 
-    const [getWrite, setWrite] = useState([]);
-    const text = useRef();
-    const send = () => {
-        let chatData = text.current.value;
-        socket.emit('msg', chatData);
-        // setWrite(getWrite => [...getWrite])
-    }
+  const [getWrite, setWrite] = useState([])
+  const text = useRef()
+  const send = () => {
+    let chatData = text.current.value
+    socket.emit('msg', chatData)
+    // setWrite(getWrite => [...getWrite])
+  }
 
-    useEffect(()=>{
-        socket.on('msg', data => {
-            console.log('돈다')
-            setWrite(list => [...list, {data}]);
-        });
-       
-    },[socket])
+  useEffect(() => {
+    socket.on('msg', (data) => {
+      console.log('돈다')
+      setWrite((list) => [...list, { data }])
+    })
+  }, [socket])
 
-    return(
-        <Grid is_flex width='100vw' height='100vh'>
-            <Grid width='75vw' bg='pink'></Grid>
-            <Grid width='500px' padding='5% 10px 5% 10px'>
-                <Chatbox>
-                    <Grid width='100%' height='10%'></Grid>
-                    <Grid height='75%' bg='Lemonchiffon'>
-                        { getWrite.map((e) => {
-                            return(
-                            <Chatdiv currentId={currentId} e={e}/>
-                            )
-                        })}
-                    </Grid>
-                    <Grid padding='5%' height='15%'>
-                        <input ref={text} style={{borderRadius:'10px', padding:'10px',border:'none', width:'80%', fontSize:'21px'}}/>
-                        <Button _onClick={()=>{send()}}>보내기</Button>
-                    </Grid>
-                </Chatbox>
-            </Grid>
-        </Grid>
-    )
+  return (
+    <Grid is_flex width="100vw" height="100vh">
+      <Grid width="75vw" bg="pink">
+        <VideoChat />
+      </Grid>
+      <Grid width="500px" padding="5% 10px 5% 10px">
+        <Chatbox>
+          <Grid width="100%" height="10%"></Grid>
+          <Grid height="75%" bg="Lemonchiffon">
+            {getWrite.map((e) => {
+              return <Chatdiv currentId={currentId} e={e} />
+            })}
+          </Grid>
+          <Grid padding="5%" height="15%">
+            <input
+              ref={text}
+              style={{
+                borderRadius: '10px',
+                padding: '10px',
+                border: 'none',
+                width: '80%',
+                fontSize: '21px',
+              }}
+            />
+            <Button
+              _onClick={() => {
+                send()
+              }}
+            >
+              보내기
+            </Button>
+          </Grid>
+        </Chatbox>
+      </Grid>
+    </Grid>
+  )
 }
 
 const Chatbox = styled.div`
-    width:100%;
-    height:80vh;
-    background:#eee;
-    border-radius:5%;
-    box-shadow:1px 1px 1px 1px gray;
+  width: 100%;
+  height: 80vh;
+  background: #eee;
+  border-radius: 5%;
+  box-shadow: 1px 1px 1px 1px gray;
 `
 
 export default GameRoom
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { ConnectedRouter } from 'connected-react-router';
-import { Route } from 'react-router-dom';
-import { history } from '../redux/configureStore';
-import Login from '../pages/Login';
-import SignUp from '../pages/SignUp';
-import Main from '../pages/Main';
-import Gameroom from '../pages/Gameroom';
-import KakaoLogin from './KakaoLogin';
-import Loading from '../pages/Loading';
+import { ConnectedRouter } from 'connected-react-router'
+import { Route } from 'react-router-dom'
+import { history } from '../redux/configureStore'
+import Login from '../pages/Login'
+import SignUp from '../pages/SignUp'
+import Main from '../pages/Main'
+import Gameroom from '../pages/Gameroom'
+import KakaoLogin from './KakaoLogin'
+import Loading from '../pages/Loading'
 import FindPw from '../pages/FindPw'
+import VideoChat from '../component/VideoChat'
 import { useDispatch } from 'react-redux'
 
 function App() {
@@ -24,12 +25,13 @@ function App() {
   return (
     <>
       <ConnectedRouter history={history}>
-        <Route exact path='/' component={Loading}/>
+        <Route exact path="/" component={Loading} />
         <Route exact path="/gamemain" component={Main} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/findpw" component={FindPw} />
-        <Route exact path="/gameroom" component={Gameroom} />
+        <Route exact path="/gameroom/:num" component={Gameroom} />
+        <Route exact path="/video" component={VideoChat} />
         <Route exact path="/main" component={KakaoLogin} />
       </ConnectedRouter>
     </>
