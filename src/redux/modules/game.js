22f1c3c2f5c83283f2032d61b2d327ecbada5c15
Jina Kim <kjn0265@gmail.com>
2022-05-04 19:58:44 +0900

    머지2

--- src/redux/modules/game.js
+++ src/redux/modules/game.js
@@ -1,75 +1,74 @@
 import { createAction, handleActions } from 'redux-actions'
-import { produce } from 'immer';
+import { produce } from 'immer'
 
-const SEND_SOCKET = 'SEND_SOCKET';
-const NOTI = 'NOTI';
-const JOB = 'JOB';
-const KILLED = 'KILLED';
-const SURVIVED = 'SURVIVED';
-const IS_NIGHT = 'IS_NIGHT';
-const COP_SELECTED = 'COP_SELECTED';
-
+const SEND_SOCKET = 'SEND_SOCKET'
+const NOTI = 'NOTI'
+const JOB = 'JOB'
+const KILLED = 'KILLED'
+const SURVIVED = 'SURVIVED'
+const IS_NIGHT = 'IS_NIGHT'
+const COP_SELECTED = 'COP_SELECTED'
 
 const sendSocket = createAction(SEND_SOCKET, (socket) => ({ socket }))
-const noticeInfo = createAction(NOTI, (noti) => ({noti}));
-const playerJob = createAction(JOB, (job) => ({job}));
-const playerWhoKilled = createAction(KILLED, (player) => ({player}));
-const playerWhoSurvived = createAction(SURVIVED, (player) => ({player}));
-const dayAndNight = createAction(IS_NIGHT, (boolean) => ({boolean}))
-const copSelected = createAction(COP_SELECTED, (selected) => ({selected}))
+const noticeInfo = createAction(NOTI, (noti) => ({ noti }))
+const playerJob = createAction(JOB, (job) => ({ job }))
+const playerWhoKilled = createAction(KILLED, (player) => ({ player }))
+const playerWhoSurvived = createAction(SURVIVED, (player) => ({ player }))
+const dayAndNight = createAction(IS_NIGHT, (boolean) => ({ boolean }))
+const copSelected = createAction(COP_SELECTED, (selected) => ({ selected }))
 
 const initialState = {
-    socket: null,
-    noti : null,
-    job : null,
-    killed : [],
-    survived : null,
-    night : null,
-    copSelect : null,
-  }
+  socket: null,
+  noti: null,
+  job: null,
+  killed: null,
+  survived: null,
+  night: null,
+  copSelect: null,
+}
 
 export default handleActions(
-    {
-        [SEND_SOCKET]: (state, action) =>
-            produce(state, (draft) => {
-                draft.socket = action.payload.socket
-            }),
-        [NOTI]: (state, action) =>
-            produce(state, (draft) => {
-                draft.noti = action.payload.noti
-            }),
-        [JOB]: (state, action) =>
-            produce(state, (draft) => {
-                draft.job = action.payload.job
-            }),
-        [KILLED]: (state, action) =>
-            produce(state, (draft) => {
-                draft.killed = action.payload.player;
-            }),
-        [SURVIVED]: (state, action) =>
-            produce(state, (draft) => {
-                draft.survived = action.payload.player;
-            }),
-        [SURVIVED]: (state, action) =>
-            produce(state, (draft) => {
-                draft.night = action.payload.boolean;
-            }),
-        [COP_SELECTED]: (state, action) =>
-            produce(state, (draft) => {
-                draft.copSelect = action.payload.selected;
-            }),
-    },
-    initialState,
-  )
+  {
+    [SEND_SOCKET]: (state, action) =>
+      produce(state, (draft) => {
+        draft.socket = action.payload.socket
+      }),
+    [NOTI]: (state, action) =>
+      produce(state, (draft) => {
+        draft.noti = action.payload.noti
+      }),
+    [JOB]: (state, action) =>
+      produce(state, (draft) => {
+        draft.job = action.payload.job
+      }),
+    [KILLED]: (state, action) =>
+      produce(state, (draft) => {
+        draft.killed = action.payload.player
+      }),
+    [SURVIVED]: (state, action) =>
+      produce(state, (draft) => {
+        draft.survived = action.payload.player
+      }),
+    [SURVIVED]: (state, action) =>
+      produce(state, (draft) => {
+        draft.night = action.payload.boolean
+      }),
+    [COP_SELECTED]: (state, action) =>
+      produce(state, (draft) => {
+        draft.copSelect = action.payload.selected
+      }),
+  },
+  initialState,
+)
 
-  const actionCreators = {
-    sendSocket,
-    noticeInfo,
-    playerJob,
-    playerWhoKilled,
-    playerWhoSurvived,
-    dayAndNight,
-    copSelected,
-  }
-  
-  export { actionCreators }
+const actionCreators = {
+  sendSocket,
+  noticeInfo,
+  playerJob,
+  playerWhoKilled,
+  playerWhoSurvived,
+  dayAndNight,
+  copSelected,
+}
+
+export { actionCreators }
