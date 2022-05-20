9866beb0da13dcd26a5cc8987cd34176cb7a0a8a
Jina Kim <kjn0265@gmail.com>
2022-05-21 06:53:51 +0900

    반응형

--- src/styles/mediaQuery.js
+++ src/styles/mediaQuery.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import MediaQuery from 'react-responsive'
-import { useMediaQuery } from 'react-responsive'
+// import React from 'react'
+// import MediaQuery from 'react-responsive'
+// import { useMediaQuery } from 'react-responsive'
 
-export const DeskTop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 1500 })
-  return isDesktop ? children : null
-}
+// export const DeskTop = ({ children }) => {
+//   const isDesktop = useMediaQuery({ minWidth: 1500 })
+//   return isDesktop ? children : null
+// }
 
-export const Tablet = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1499 })
-  return isTablet ? children : null
-}
+// export const Tablet = ({ children }) => {
+//   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1499 })
+//   return isTablet ? children : null
+// }
