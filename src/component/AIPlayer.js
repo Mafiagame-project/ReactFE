0e58db0727f9bae1f4a770123d390a6a8736c465
Jina Kim <kjn0265@gmail.com>
2022-05-27 11:35:47 +0900

    ai 추가틀

---
+++ src/component/AIPlayer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AiPlayer = () => {
+  return (
+    <>
+      <div classNmae="video_grid">
+        <div className="ai_video"></div>
+        <div className="fl">
+          <p>{userNick}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AiPlayer
