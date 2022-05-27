51fd05eda59288bc819a50f405b2b8f2354a7728
Jina Kim <kjn0265@gmail.com>
2022-05-27 20:29:08 +0900

    Update Oauth.js

--- src/shared/Oauth.js
+++ src/shared/Oauth.js
@@ -8,6 +8,16 @@ export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id
 const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID
 const NAVER_CALLBACK_URI = process.env.REACT_APP_NAVER_CALLBACK_URI
 
-const state = 'login'
+const randomString = () => {
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
+  const stringLength = 6
+  let randomstring = ''
+  for (let i = 0; i < stringLength; i++) {
+    const rnum = Math.floor(Math.random() * chars.length)
+    randomstring += chars.substring(rnum, rnum + 1)
+  }
+  return randomstring
+}
+const newState = randomString()
 
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_CALLBACK_URI}&state=${state}`
+export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_CALLBACK_URI}&state=${newState}`
